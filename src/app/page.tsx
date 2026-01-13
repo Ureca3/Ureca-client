c386783de928b59d035f1b0de63113a71928efec
박승연 <70251709+40food@users.noreply.github.com>
2026-01-13 14:01:02 +0900

    fallback component 제작 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { ErrorComponent } from '@/components/ui/common/ErrorComponent';
+import { LoadingComponent } from '@/components/ui/common/LoadingComponent';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
+        <ErrorComponent/>
+        <LoadingComponent/>
         <Image
           className="dark:invert"
           src="/next.svg"
