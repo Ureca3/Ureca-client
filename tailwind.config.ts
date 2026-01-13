f56c28f83be2d81619f6aa11e68151b41dd87053
Zoo2-bi <bagjui068@gmail.com>
2026-01-13 14:40:04 +0900

    fix: 경로수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,13 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  content: ['./src/app/**/*.{ts,tsx,js,jsx,mdx}', './src/components/**/*.{ts,tsx,js,jsx,mdx}'],
   theme: {
     extend: {
       fontFamily: {
         pretendard: ['var(--font-pretendard)'],
         gowun: ['var(--font-gowun-dodum)'],
-        kotra: ['var(--font-kotra-hope)'],
-        sans: ['system-ui', 'sans-serif'],
+        sans: ['var(--font-pretendard)', 'system-ui', 'sans-serif'],
       },
     },
   },
