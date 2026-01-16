de97c2ee61049d9a5ecb537d29bcb3bafb7ac147
Zoo2-bi <bagjui068@gmail.com>
2026-01-16 15:38:24 +0900

    feat: 요약 성공 시 상세 페이지 제작

--- src/components/summary/detail/SummaryLoadingPage.tsx
+++ src/components/summary/detail/SummaryLoadingPage.tsx
@@ -11,18 +11,13 @@ export const SummaryLoadingPage = () => {
 
   return (
     <>
-      {/* 헤더 */}
       <div className="relative flex h-14 items-center bg-white">
-        {/* 뒤로가기 */}
         <button type="button" onClick={() => router.push('/summary')} className="ml-4">
           <Image src={Back} alt="돌아가기" width={20} height={20} priority />
         </button>
-
-        {/* 중앙 타이틀 */}
         <p className="absolute left-1/2 -translate-x-1/2 text-sm font-semibold">상담 요약</p>
       </div>
 
-      {/* 본문 */}
       <div className="mx-auto mt-10 flex min-h-[450px] w-[335px] flex-col items-center justify-center rounded-2xl bg-white p-6 shadow-lg">
         <Image src={SummaryLoading} alt="요약 중" width={201} height={223} priority />
 
