dcdc162dc69197b5be5578992b7b6608fee5b2e2
40food <40food@naver.com>
2026-01-17 22:23:36 +0900

    [URECA-30] feat: fallback component 추가

--- src/components/ui/fallback/ErrorComponent.tsx
+++ src/components/ui/fallback/ErrorComponent.tsx
@@ -1,23 +1,27 @@
-"use client"
+'use client';
 import React from 'react';
-import sorry from '@/assets/images/mooner/sorry.png';
-import Image from 'next/image';
-import { Button } from '../button';
 import { useRouter } from 'next/navigation';
 
+import Sorry from '@/assets/fallback/mooner_sorry.svg';
+
+import { Button } from '../button';
+
 export const ErrorComponent = () => {
-    const router=useRouter();
-    return (
-        <div className='w-screen h-screen flex flex-col items-center justify-center'>
-            <Image src={sorry} alt="sorry" width={183}/>
-            <p className='font-gowun text-2xl mt-10'>요청하신 페이지를 찾을 수 없습니다.</p>
-            <Button variant={'solid'}
-            tone={'primary'}
-            size={'m'}
-            children={"뒤로가기"} className='mt-5'
-            onClick={()=>{
-                router.back();
-            }}/>
-        </div>
-    );
-}; 
+  const router = useRouter();
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
+      <Sorry />
+      <p className="font-gowun mt-10 text-2xl">요청하신 페이지를 찾을 수 없습니다.</p>
+      <Button
+        variant={'solid'}
+        tone={'primary'}
+        size={'m'}
+        children={'뒤로가기'}
+        className="mt-5"
+        onClick={() => {
+          router.back();
+        }}
+      />
+    </div>
+  );
+};
--- src/components/ui/fallback/LoadingComponent.tsx
+++ src/components/ui/fallback/LoadingComponent.tsx
@@ -1,17 +1,27 @@
-import Image from 'next/image';
 import React from 'react';
-import wink from '@/assets/images/mooner/wink.png'
-import '@/styles/animation.css'
+
+import Wink from '@/assets/fallback/mooner_wink.svg';
+
+import '@/styles/animation.css';
 
 export const LoadingComponent = () => {
-    return (
-        <div className='w-screen h-screen flex flex-col items-center justify-center'>
-            <Image src={wink} alt='wink' width={227}/>
-            <div className="flex justify-center items-center space-x-4 mt-10">
-                <span className="w-4 h-4 bg-gray rounded-full animate-(--animate-bounce-color)" style={{ animationDelay: '0s' }}></span>
-                <span className="w-4 h-4 bg-gray rounded-full animate-(--animate-bounce-color)" style={{ animationDelay: '0.2s' }}></span>
-                <span className="w-4 h-4 bg-gray rounded-full animate-(--animate-bounce-color)" style={{ animationDelay: '0.4s' }}></span>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
+      <Wink />
+      <div className="mt-10 flex items-center justify-center space-x-4">
+        <span
+          className="bg-gray h-4 w-4 animate-(--animate-bounce-color) rounded-full"
+          style={{ animationDelay: '0s' }}
+        ></span>
+        <span
+          className="bg-gray h-4 w-4 animate-(--animate-bounce-color) rounded-full"
+          style={{ animationDelay: '0.2s' }}
+        ></span>
+        <span
+          className="bg-gray h-4 w-4 animate-(--animate-bounce-color) rounded-full"
+          style={{ animationDelay: '0.4s' }}
+        ></span>
+      </div>
+    </div>
+  );
 };
