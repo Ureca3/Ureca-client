73a991e8c786b4eed52c5d78cd84d0d0a88476af
joonhyong <dbshwns0528@naver.com>
2026-01-17 14:40:56 +0900

    [URECA-31] feat: 로그인 모달 하단에 안내문구 추가

--- src/components/auth/LoginModal.tsx
+++ src/components/auth/LoginModal.tsx
@@ -18,15 +18,21 @@ export const LoginModal = ({ onClose }: Props) => {
         <button type="button" onClick={onClose} className="text-gray absolute top-4 right-4">
           <Cross className="hover:opacity-50" />
         </button>
+
         <div className="flex flex-col gap-4">
           <div className="flex justify-center">
             <Logo />
           </div>
+
           <h2 className="mt-4 mb-6 text-center text-2xl font-bold text-black">유니티 로그인</h2>
 
           <GoogleLoginButton />
           <KakaoLoginButton />
           <NaverLoginButton />
+
+          <p className="flex justify-end text-xs font-semibold text-black">
+            U+NITY는 회원 전용 앱 입니다.
+          </p>
         </div>
       </div>
     </div>
