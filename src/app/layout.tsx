ac747c6d9eba9d685bf15cb5cc51d223126cb5ff
Zoo2-bi <bagjui068@gmail.com>
2026-01-19 10:06:22 +0900

    fix: 배경 컬러 고정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           geistMono.variable,
           pretendard.variable,
           gowunDodum.variable,
-          'bg-[#f6e8f6]',
+          'bg-[#FBF8FB]',
           'min-h-screen',
         ].join(' ')}
       >
