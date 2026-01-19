8e3d4a8f626287250f02ad841fa308b39649a35e
박주이 <bagjui068@gmail.com>
2026-01-19 11:10:32 +0900

    [URECA-32] Feat: 요약 페이지 퍼블리싱 (#29)
    * [URECA-17] feat: 네비바 헤더 생성
    
    * feat: 요약 페이지 제작을 위한 컴포넌트 제작
    
    * feat: summary 페이지의 page.tsx 작성
    
    * fix: 컬러 시스템 white색상 변경 및 eslint 수정
    
    * feat: 요약 성공 시 상세 페이지 제작
    
    * fix: 불필요코드 제거 및 색상 코드 global & layout 배경색 충돌 해결
    
    * fix: 유효성 문제 해결
    
    * fix: 배경 컬러 고정
    
    * fix: layout dvh로 변경
    
    * fix: page 자체 컴포넌트 페이지 위치 변경
    
    * fix: 폴더명 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,6 +38,8 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           geistMono.variable,
           pretendard.variable,
           gowunDodum.variable,
+          'bg-[#FBF8FB]',
+          'min-h-dvh',
         ].join(' ')}
       >
         <Providers>
