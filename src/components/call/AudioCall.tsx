2b814ef26d449024dc7d79d47c1f4cdf5fce427e
40food <40food@naver.com>
2026-01-20 18:26:54 +0900

    [URECA-40] fix: develop 머지 충돌 오류 해결

---
+++ src/components/call/AudioCall.tsx
@@ -0,0 +1,11 @@
+// CORE-SDK
+// import AgoraUIKit from 'agora-rn-uikit';
+
+// export const AudioCall = () => {
+//   const connectionData = {
+//     appId: 'e7f6e9aeecf14b2ba10e3f40be9f56e7',
+//     channel: 'test',
+//     token: null, // enter your channel token as a string
+//   };
+//   return <AgoraUIKit connectionData={connectionData} />;
+// };
