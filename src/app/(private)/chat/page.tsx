cad3218db79a790672ebb043713e3afa2a7781cf
Jiyeon <xhdrp@sookmyung.ac.kr>
2026-01-20 15:42:26 +0900

    [URECA-43] fix: 정적 배포수정

--- src/app/(private)/chat/page.tsx
+++ src/app/(private)/chat/page.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import dynamicImport from 'next/dynamic';
 
 import { ChattingFooter } from '@/components/chat/chatting-footer';
-import { ChattingScreen } from '@/components/chat/chatting-screen';
 
-const page = () => {
+export const dynamic = 'force-static';
+
+const ChattingScreen = dynamicImport(
+  () => import('@/components/chat/chatting-screen').then((m) => m.ChattingScreen),
+  { ssr: false },
+);
+
+export default function Page() {
   return (
     <div className="flex h-screen flex-col">
       <ChattingScreen />
       <ChattingFooter />
     </div>
   );
-};
-
-export default page;
+}
