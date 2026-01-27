3aa2bf4d12c5d87c2abcbe1e513afdf5db44d829
Jiyeon <xhdrp@sookmyung.ac.kr>
2026-01-27 14:47:02 +0900

    fix: 헤더 수정

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -3,16 +3,16 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import Chat from '@/assets/icons/header/Chat.png';
 import Logo from '@/assets/icons/header/Logo.png';
+import Call from '@/assets/icons/header/Phone.svg';
 
 export const Header = () => {
   return (
     <header className="flex h-14 items-center justify-between bg-white">
       <Image src={Logo} alt="U+NITY 로고" width={72} height={24} priority className="ml-2" />
 
-      <Link href="/chat">
-        <Image src={Chat} alt="채팅" width={40} height={40} className="cursor-pointer" />
+      <Link href="/call">
+        <Image src={Call} alt="상담하기" width={40} height={40} className="cursor-pointer" />
       </Link>
     </header>
   );
