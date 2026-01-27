03c03e09d2525cf93f77ad5ed4c38a9a54845e10
joonhyong <dbshwns0528@naver.com>
2026-01-27 19:09:11 +0900

    [URECA-69] feat: 헤더바 로고에 메인페이지로 리다이렉트 기능 추가

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -9,7 +9,9 @@ import Call from '@/assets/icons/header/Phone.svg';
 export const Header = () => {
   return (
     <header className="flex h-14 items-center justify-between bg-white px-3">
-      <Image src={Logo} alt="U+NITY 로고" width={72} height={24} priority className="ml-2" />
+      <Link href="/">
+        <Image src={Logo} alt="U+NITY 로고" width={72} height={24} priority className="ml-2" />
+      </Link>
 
       <Link href="/call">
         {/* <Image src={Call} alt="상담하기" width={40} height={40} className="cursor-pointer" /> */}
