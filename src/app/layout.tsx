a12412208573cb70ab44bf416a1ff4f7880bb673
joonhyong <dbshwns0528@naver.com>
2026-01-28 19:42:55 +0900

    [URECA-77] fix: hydration 오류 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         ].join(' ')}
       >
         <Providers>
-          <main>{children}</main>
+          {children}
           <GlobalComponents />
         </Providers>
       </body>
