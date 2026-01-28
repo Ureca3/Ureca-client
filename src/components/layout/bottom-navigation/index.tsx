8eafac15ef3d9f4cd083f4bc337dc24cb3f41651
joonhyong <dbshwns0528@naver.com>
2026-01-28 21:39:30 +0900

    [URECA-77] chore: 네비바 아이콘 png에서 svg로 변경

--- src/components/layout/bottom-navigation/index.tsx
+++ src/components/layout/bottom-navigation/index.tsx
@@ -1,19 +1,18 @@
 'use client';
 
-import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
-import Home from '@/assets/icons/nav/Home.png';
-import My from '@/assets/icons/nav/My.png';
-import Recommend from '@/assets/icons/nav/Recommend.png';
-import SummaryMain from '@/assets/icons/nav/SummaryMain.png';
+import My from '@/assets/icons/nav/circle-user-round.svg';
+import Summary from '@/assets/icons/nav/file-text.svg';
+import Recommend from '@/assets/icons/nav/gift.svg';
+import Home from '@/assets/icons/nav/house.svg';
 import Call from '@/assets/images/call/call.svg';
 
 export const BottomNav = () => {
   const router = useRouter();
 
   return (
-    <nav className="fixed bottom-4 left-1/2 z-50 w-[360px] -translate-x-1/2 rounded-2xl border-[#61626F] bg-white px-6 py-1.5 shadow-xl">
+    <nav className="fixed bottom-4 left-1/2 z-50 w-[90%] max-w-3xl -translate-x-1/2 rounded-2xl border-[#61626F] bg-white px-6 py-1.5 shadow-xl">
       <ul className="flex items-end justify-between">
         {/* 홈 */}
         <li>
@@ -22,8 +21,8 @@ export const BottomNav = () => {
             onClick={() => router.push('/')}
             className="flex flex-col items-center gap-1"
           >
-            <Image src={Home} alt="홈" width={15} height={15} />
-            <span className="text-xs text-gray-400">홈</span>
+            <Home className="h-5 w-5 text-gray-600" />
+            <span className="text-xs text-gray-600">홈</span>
           </button>
         </li>
 
@@ -34,10 +33,11 @@ export const BottomNav = () => {
             onClick={() => router.push('/recommend')}
             className="flex flex-col items-center gap-1"
           >
-            <Image src={Recommend} alt="추천" width={15} height={15} />
-            <span className="text-xs text-gray-400">추천</span>
+            <Recommend className="h-5 w-5 text-gray-600" />
+            <span className="text-xs text-gray-600">추천</span>
           </button>
         </li>
+
         {/* 상담 */}
         <li className="-mt-8">
           <button
@@ -47,7 +47,6 @@ export const BottomNav = () => {
           >
             <div className="flex h-14 w-14 items-center justify-center rounded-full border-2 border-white bg-[#E30084] shadow-lg">
               <Call />
-              {/* <Image src={Chatting} alt="상담" width={21} height={21} /> */}
             </div>
             <span className="text-xs font-semibold text-[#E30084]">상담</span>
           </button>
@@ -60,20 +59,20 @@ export const BottomNav = () => {
             onClick={() => router.push('/summary')}
             className="flex flex-col items-center gap-1"
           >
-            <Image src={SummaryMain} alt="요약" width={15} height={15} />
-            <span className="text-xs text-gray-400">요약</span>
+            <Summary className="h-5 w-5 text-gray-600" />
+            <span className="text-xs text-gray-600">요약</span>
           </button>
         </li>
 
-        {/* MY */}
+        {/* 마이 */}
         <li>
           <button
             type="button"
             onClick={() => router.push('/mypage')}
             className="flex flex-col items-center gap-1"
           >
-            <Image src={My} alt="MY" width={15} height={15} />
-            <span className="text-xs text-gray-400">MY</span>
+            <My className="h-5 w-5 text-gray-600" />
+            <span className="text-xs text-gray-600">MY</span>
           </button>
         </li>
       </ul>
